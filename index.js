6ee9fab0522341e15e4fd19cdbd386caa8887977
Shanjida Rahman Maria <shanjidamaria07@gmail.com>
2022-04-29 11:28:18 +0600

    Spicy-Velvet-Server connection with Mongodb successful

--- index.js
+++ index.js
@@ -1,12 +1,42 @@
 const express = require("express");
 const app = express();
+const cors = require("cors");
+require("dotenv").config();
+const { MongoClient, ServerApiVersion } = require("mongodb");
 const port = process.env.PORT || 5000;
 
 //middleware
 app.use(cors());
 app.use(express.json());
 
-app.get("/", (res, req) => {
+const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.cog8e.mongodb.net/Spicy-Velvet?retryWrites=true&w=majority`;
+console.log(uri);
+const client = new MongoClient(uri, {
+  useNewUrlParser: true,
+  useUnifiedTopology: true,
+  serverApi: ServerApiVersion.v1,
+});
+
+async function run() {
+  try {
+    await client.connect();
+    const productCollection = client.db("Spicy-Velvet").collection("products");
+
+    app.get("/products", async (req, res) => {
+      const query = {};
+      const cursor = productCollection.find(query);
+      const products = await cursor.toArray();
+      console.log(products);
+      res.send(products);
+    });
+  } finally {
+  }
+}
+
+run().catch(console.dir);
+
+// root
+app.get("/", (req, res) => {
   res.send("Spicy Velvet warehouse website running");
 });
 
